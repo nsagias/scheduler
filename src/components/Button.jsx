66586a92c59b21b3fbf0e1259fae2b554b01b370
Nick <nsagias@gmail.com>
2021-10-27 01:22:21 -0400

    Refactor and use className library

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,21 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-  let buttonClass = "button";
-  if (props.confirm) {
-    buttonClass += " button--confirm";
-  }
-  if (props.danger) {
-    buttonClass += " button--danger";
-  }
-  
+  // let buttonClass = "button";
+  // if (props.confirm) {
+  //   buttonClass += " button--confirm";
+  // }
+  // if (props.danger) {
+  //   buttonClass += " button--danger";
+  // }
+  const {confirm, danger} = props
+  let buttonClass = classNames("button", {
+    "button--confirm": confirm,
+    "button--danger": danger
+  });
+ 
   return (
     <button 
       className={buttonClass}
