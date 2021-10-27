b3937595a307445998210b897480a0f3bd8f087b
Nick <nsagias@gmail.com>
2021-10-27 17:40:40 -0400

    Add code to make it work

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import classNames from "classnames";
+import "components/DayListItem.scss"
 
 export default function DayListItem(props) {
+  const {selected, spots, name} = props;
+
+  // const dayClass = classNames('li', {
+  //   "day-list__item": true,
+  //   "--selected": selected,
+  //   "--full": spots,
+  // });
+  const dayClass = classNames("day-list__item", {
+    // "day-list__item": true,
+    "day-list__item--selected": selected,
+    "day-list__item--full": spots === 0,
+  });
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+    <li className={dayClass} 
+       onClick={() => props.setDay(name)}>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{spots} spots remaining</h3>
     </li>
   );
 }
