a90c9088735826c12a11f14b7ccb142be6e68480
Nick <nsagias@gmail.com>
2021-10-26 23:36:53 -0400

    Add content to git button class, add button scss, rename button and app with JSX extention

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "components/Button.scss";
+
+export default function Button(props) {
+  let buttonClass = "button";
+  if (props.confirm) {
+    buttonClass += " button--confirm"
+  }
+
+  return <button className={buttonClass}>{props.children}</button>;
+}
+
