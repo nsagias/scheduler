a07ae8b723ed0c172d75b027a988219a2c40a9ab
Nick <nsagias@gmail.com>
2021-10-27 18:17:00 -0400

    Add testing and Add formatSpots functions to format output based on number of spots

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -4,23 +4,27 @@ import "components/DayListItem.scss"
 
 export default function DayListItem(props) {
   const {selected, spots, name} = props;
-
-  // const dayClass = classNames('li', {
-  //   "day-list__item": true,
-  //   "--selected": selected,
-  //   "--full": spots,
-  // });
   const dayClass = classNames("day-list__item", {
     // "day-list__item": true,
     "day-list__item--selected": selected,
     "day-list__item--full": spots === 0,
   });
 
+  function formatSpots(spots) {
+    if (spots === 0) {
+     return `no spots remaining`;
+    }
+    if (spots === 1) {
+     return `${spots} spot remaining`;
+    }
+    return `${spots} spots remaining`;
+  };
+
   return (
     <li className={dayClass} 
        onClick={() => props.setDay(name)}>
       <h2 className="text--regular">{name}</h2>
-      <h3 className="text--light">{spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
   );
 }
