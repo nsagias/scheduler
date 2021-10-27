7aaad0cb5c6772ebab1b20c04db45b47d854eaf9
Nick <nsagias@gmail.com>
2021-10-27 19:32:36 -0400

    Creat DayList coponent, no code yet

---
+++ src/components/DayList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+//   Our <DayList> component will take in three props.
+
+// days:Array an array of objects (each object represents a day and includes an id, name, and spots)
+// day:String the currently selected day
+// setDay:Function sets the currently sele
+export default function DayList(props) {
+  
+  return <u></u>;
+}
