12e2e884f49d8b381be5ecc3c1ac9ccedb49f795
Nick <nsagias@gmail.com>
2021-10-28 01:43:19 -0400

    Add onclick, and state

--- src/components/InterviewListItem.jsx
+++ src/components/InterviewListItem.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { useState } from 'react';
+
 import "components/InterviewListItem.scss"
 
 export default function InterviewListItem(props){
   const {id, name, avatar} = props;
+  const [interview, setInterviewer] = useState("Sylvia Palmer");
   
   return (
     <li 
     className="interviewers__item"
-    onClick={() => console.log('clicked')}>
+    onClick={() => setInterviewer(id)}>
       <img
         className="interviewers__item-image"
         src={avatar}
-        alt="Sylvia Palmer"
+        alt={name}
       />
     {name}
   </li>
