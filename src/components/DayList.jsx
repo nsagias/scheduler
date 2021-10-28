9eb838d3ae05bf2264f513ef6236e24efc2f5f27
Nick <nsagias@gmail.com>
2021-10-27 20:15:19 -0400

    Render days in DayList

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import DayListItem from './DayListItem';
 //   Our <DayList> component will take in three props.
 
 // days:Array an array of objects (each object represents a day and includes an id, name, and spots)
 // day:String the currently selected day
-// setDay:Function sets the currently sele
+// setDay:Function sets the currently selected day and accepts the name of the day eg. "Monday", "Tuesday
+
 export default function DayList(props) {
-  
-  return <u></u>;
+  const { days, setDay } = props; 
+  console.log(days)
+  const parsedDay = days.map(days => <DayListItem key={days.id}{...days}  setDay />);
+  return (
+  <ul>
+    {parsedDay}
+  </ul>
+  );
 }
