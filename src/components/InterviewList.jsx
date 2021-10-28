79d2a29d51efe6a6f43b7174eb38e04873021be1
Nick <nsagias@gmail.com>
2021-10-28 14:47:15 -0400

    Add example props data as noted, to be deleted once done

--- src/components/InterviewList.jsx
+++ src/components/InterviewList.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
+// const interviewers = [
+//   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+//   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+//   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+//   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+//   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+// ];
+
+// Our InterviewerList receives three props:
+
+// interviewers:array - an array of objects as seen above
+// setInterviewer:function - a function that accepts an interviewer id. This function will simply be passed down to the InterviewerListItem
+// interviewer:number - a number that represents the id of the currently selected interviewer
+
 export default function InterviewList(props) {
   return (
   <section className="interviewers">
