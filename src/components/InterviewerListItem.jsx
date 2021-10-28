cc7abc857c4324c473c5a06b7618ac2ba386b727
Nick <nsagias@gmail.com>
2021-10-28 19:34:55 -0400

    Change logic from short circuit to function to evaluate selected and whether to show name

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -5,28 +5,33 @@ import { useState } from 'react';
 import "components/InterviewListItem.scss"
 
 export default function InterviewListItem(props){
-  const {id, name, avatar, selected} = props;
+  let {id, name, avatar, selected} = props;
   const [interviewer, setInterviewer] = useState("");
-  console.log('props',props)
-  console.log('get selected',selected)
-  // console.log('interviewer', interviewer)
-  const interviewerClass = classNames("interviewers__item", {
+
+  function nameLogic(name) {
+    if (!selected) {
+      return undefined;
+    }
+    return name;
+  };
+  const interviewersClass = classNames("interviewers__item", {
     "interviewers__item--selected": selected,
 
   });
+ 
   return (
     <li 
-    className={interviewerClass}
+      className={interviewersClass}
       // className="interviewers__item"
-      onClick={() => setInterviewer(id)}>
+      onClick={() => setInterviewer(id)}
+      >
       <img
         className="interviewers__item-image"
         src={avatar}
-        alt={name && name}
+        alt={nameLogic(name)}
+
       />
-      {/* check if name equal selected and then show */}
-    {name && name}
-   
+      {nameLogic(name)}
   </li>
   );
 }
