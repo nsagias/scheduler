ff622ad40a67023c14dca7a7b384ea6ab6a0f6cf
Nick <nsagias@gmail.com>
2021-10-29 18:04:08 -0400

    create Show.jsx and import into stories/indexjs

---
+++ src/components/Appointment/Show.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Show(props) {
+  return (
+    <h1></h1>
+  );
+}
--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 storiesOf("Button", module)
   .addParameters({
