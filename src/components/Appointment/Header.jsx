ee164c623e4004c298636c2080b10e4674ec5277
Nick <nsagias@gmail.com>
2021-10-29 17:44:19 -0400

    add props.time and call props in Header

--- src/components/Appointment/Header.jsx
+++ src/components/Appointment/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export default function Header(props) {
+  const {time} = props;
+  console.log(props.time)
   return (
     <header className="appointment__time">
-      <h4 className="text--semi-bold">12pm</h4>
+      <h4 className="text--semi-bold">{time}</h4>
       <hr className="appointment__separator" />
    </header>
   );
