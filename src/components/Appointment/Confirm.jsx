4c59b31f3edcce435bd7120bcf208abaf7e4b7c8
Nick <nsagias@gmail.com>
2021-10-30 14:36:40 -0400

    remove console.log for props

--- src/components/Appointment/Confirm.jsx
+++ src/components/Appointment/Confirm.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Button from 'components/Button';
 
 export default function Confirm(props) {
-  console.log(props) 
   const {message, onCancel, onConfirm} = props;
   return (
     <main className="appointment__card appointment__card--confirm">
