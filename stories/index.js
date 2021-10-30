9847d93737917cc4218874101b76ff732b7749b1
Nick <nsagias@gmail.com>
2021-10-30 13:50:30 -0400

    Add confirm stories

--- stories/index.js
+++ stories/index.js
@@ -201,11 +201,17 @@ storiesOf("InterviewerList", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm"/>)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
-  .add("Show", () => <Show 
-                          student={"Lydia Miller-Jones"}
-                          // use interviewer object areard
-                          interviewer={interviewer}
-                          onEdit={action("onEdit")}
-                          onDelete={action("onDelete")}
-                          />)
-  
+  .add("Show", () => 
+    <Show 
+      student={"Lydia Miller-Jones"}
+      // use interviewer object areard
+      interviewer={interviewer}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+    />)
+  .add("Confirm", () => 
+    <Confirm
+      message={"Delete the appointment"}
+      onConfirm={ action("onConfirm") }
+      onCancel= { action("onCancel") }
+    />)
