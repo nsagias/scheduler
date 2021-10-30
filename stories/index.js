e00849d497a36bb02ddc0e0f7da6f143e106ce17
Nick <nsagias@gmail.com>
2021-10-30 19:01:47 -0400

    add form stories

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -223,3 +224,17 @@ storiesOf("InterviewerList", module)
       message={"Could Not delete appointment."}
       onClose={action("onClose")}
     />)
+  .add("Edit", () => 
+    <Form 
+      student={"Lydia Miller-Jones"}
+      interviwer={interviewer}
+      onSave={ action("onSave")}
+      onCancel= { action("onCancel") }
+    /> )
+  .add("Create", () => 
+    <Form 
+      interviewers={interviewers}
+      onSave={ action("onSave")}
+      onCancel= { action("onCancel") }   
+    />)
+
