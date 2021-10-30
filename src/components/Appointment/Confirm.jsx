be8eea76b968ba9abfb9c81c5783077f46b669f9
Nick <nsagias@gmail.com>
2021-10-30 13:58:32 -0400

    Add props onClick with onCancel and onConfirm

--- src/components/Appointment/Confirm.jsx
+++ src/components/Appointment/Confirm.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import Button from 'components/Button';
 
 export default function Confirm(props) {
+  console.log(props) 
+  const {message, onCancel, onConfirm} = props;
   return (
     <main className="appointment__card appointment__card--confirm">
-      <h1 className="text--semi-bold">Delete the appointment?</h1>
+      <h1 className="text--semi-bold">{message}</h1>
       <section className="appointment__actions">
-        <Button danger>Cancel</Button>
-        <Button danger>Confirm</Button>
+        <Button 
+          danger
+          onClick={onCancel}
+        >Cancel</Button>
+        <Button 
+          danger
+          onClick={onConfirm}
+        >Confirm</Button>
       </section>
    </main>
   );
