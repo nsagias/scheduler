a207f80129f1431a631d0757a70d52e6892ab4c9
Nick <nsagias@gmail.com>
2021-10-30 14:46:03 -0400

    Create Error component and import it in stories/indexjs

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -217,3 +218,4 @@ storiesOf("InterviewerList", module)
       onCancel= { action("onCancel") }
     />)
   .add("Status", () => <Status message={"Deleting"} />)
+  .add("Error", () => <Error />)
