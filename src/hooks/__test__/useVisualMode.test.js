ff1f167f50a3a4d95c7b720adcd5dd551918dacc
Nick <nsagias@gmail.com>
2021-11-02 15:42:04 -0400

    Add new test case second

--- src/hooks/__test__/useVisualMode.test.js
+++ src/hooks/__test__/useVisualMode.test.js
@@ -10,3 +10,10 @@ test("useVisualMode should initialize with default value", () => {
 
   expect(result.current.mode).toBe(FIRST);
 });
+
+test("useVisualMode should transition to another mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+});
