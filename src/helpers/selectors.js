686fc311cee5ebdad62663e4607b1020d24fb358
Nick <nsagias@gmail.com>
2021-11-02 01:03:04 -0400

    Add data notes

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -12,3 +12,33 @@ export function getAppointmentsForDay(state, day) {
 
   return appointments;
 }
+
+
+// expected output
+// {  
+//   "student": "Lydia Miller-Jones",
+//   "interviewer": {  
+//     "id": 1,
+//     "name": "Sylvia Palmer",
+//     "avatar": "https://i.imgur.com/LpaY82x.png"
+//   }
+// }
+
+// const interviewers = {
+//   "1": {  
+//     "id": 1,
+//     "name": "Sylvia Palmer",
+//     "avatar": "https://i.imgur.com/LpaY82x.png"
+//   },
+//   "2": {
+//     id: 2,
+//     name: "Tori Malcolm",
+//     avatar: "https://i.imgur.com/Nmx0Qxo.png"
+//   }
+// };
+
+export function getInterview(state, interview)  {
+  let result = {};
+
+  return result;
+}
