b22985bf581211a0ef05cbd7a3b003f3d626591f
Nick <nsagias@gmail.com>
2021-11-02 18:37:06 -0400

    Add back() function to move back and update state with new mode

--- src/hooks/useVisualMode.jsx
+++ src/hooks/useVisualMode.jsx
@@ -4,10 +4,26 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
   console.log('state', mode)
+  console.log(history)
   
   
-  const transition = mode => setMode(mode);
-
-  return { mode, transition };
+  const transition = mode => {
+    return (
+      setMode(mode),
+      setHistory([...history, mode])
+    );
+  };
+  
+  const back = () => {
+    const result = [...history];
+    result.pop()
+    return (
+      setHistory(() => ([...result])),
+      setMode(result[result.length -1])
+    );
+  };
+  
+  return { mode, transition, back };
 }
