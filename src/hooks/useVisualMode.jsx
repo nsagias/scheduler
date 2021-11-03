e41954da4847587867c71c16e9561762cf78f667
Nick <nsagias@gmail.com>
2021-11-02 22:15:59 -0400

    Add replace, refactor transition to copy array

--- src/hooks/useVisualMode.jsx
+++ src/hooks/useVisualMode.jsx
@@ -7,24 +7,27 @@ export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
   console.log('state', mode)
   console.log(history)
-  
-  
-  const transition = mode => {
-    return (
-      setMode(mode),
-      setHistory([...history, mode])
-    );
+
+
+  const transition = (mode, replace = false) => {
+    const result = [...history];
+    if (replace === true) {
+    result.pop()
+    }
+    setMode(() => (mode));
+    setHistory(() => ([...result, mode]));
+
   };
-  
+
   const back = () => {
-    let result = [...history];
+    const result = [...history];
     if (result.length > 1) {
       result.pop();
-    } 
-    setHistory(() => ([...result])),
-    setMode(result[result.length -1])
-    
+    }
+    setHistory(() => ([...result]));
+    setMode(() => (result[result.length - 1]));
+
   };
-  
+
   return { mode, transition, back };
 }
