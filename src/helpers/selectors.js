1763fe0a4182abf6322943e30e89862ecc328e4d
Nick <nsagias@gmail.com>
2021-11-03 16:24:12 -0400

    Add getInterviewersForDay to selecters, import into tests, modify test data, Add test

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -23,12 +23,27 @@ export function getInterview(state, interview ){
 //   result['student'] = state.appointments[3].interview['student']
 //   result['interviewer'] =interviewers[state.appointments[3].interview['interviewer']]
 // }
+// console.log('get interview',interview.interviewer)
+// console.log('student', interview.student)
+// console.log('all the interviews',state.interviewers[interview.interviewer])
 
   result['student'] = interview['student']
   result['interviewer'] = state.interviewers[interview.interviewer];
   
-console.log('get interview',interview.interviewer)
-console.log('student', interview.student)
-console.log('all the interviews',state.interviewers[interview.interviewer])
+
 return result;
 };
+
+export function getInterviewersForDay(state, day) {
+  let interviewers = [];
+ 
+  for ( const i of state.days) {  
+    if (i.name === day) { 
+      for (const x of i.interviewers) {   
+        interviewers.push(state.interviewers[x]) 
+      }
+    }
+  }
+
+  return interviewers;
+}
--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -1,7 +1,10 @@
 import { 
   getAppointmentsForDay,
-  getInterview 
+  getInterview,
+  getInterviewersForDay
 } from "helpers/selectors";
+
+
 
 
 const state = {
@@ -9,12 +12,14 @@ const state = {
     {
       id: 1,
       name: "Monday",
-      appointments: [1, 2, 3]
+      appointments: [1, 2, 3],
+      interviewers: [1, 2]
     },
     {
       id: 2,
       name: "Tuesday",
-      appointments: [4, 5]
+      appointments: [4, 5],
+      interviewers: [1, 2]
     }
   ],
   appointments: {
@@ -24,14 +29,14 @@ const state = {
     "3": {
       id: 3,
       time: "2pm",
-      interview: { student: "Archie Cohen", interviewer: 2 }
+      interview: { student: "Archie Cohen", interviewer: 2 } // Tori
     },
     // Tuesday
     "4": { id: 4, time: "3pm", interview: null },
     "5": {
       id: 5,
       time: "4pm",
-      interview: { student: "Chad Takahashi", interviewer: 2 }
+      interview: { student: "Chad Takahashi", interviewer: 2 } // Tori
     }
   },
   interviewers: {
@@ -47,6 +52,7 @@ const state = {
     }
   }
 };
+
 
 test("getAppointmentsForDay returns an array", () => {
   const result = getAppointmentsForDay(state, "Monday");
@@ -75,6 +81,37 @@ test("getAppointmentsForDay returns an empty array when the day is not found", (
 });
 
 
+// getInterviewersForDay
+test("getInterviewersForDay returns an array", () => {
+  const result = getInterviewersForDay(state, "Monday");
+  expect(Array.isArray(result)).toBe(true);
+});
+
+test("getInterviewersForDay returns an array with a length matching the number of appointments for that day", () => {
+  const result = getInterviewersForDay(state, "Monday");
+  expect(result.length).toEqual(2);
+});
+
+test("getInterviewersForDay returns an array containing the correct appointment objects", () => {
+  const [first, second] = getInterviewersForDay(state, "Tuesday");
+  expect(first).toEqual(state.interviewers["1"]);
+  expect(second).toEqual(state.interviewers["2"]);
+});
+
+test("getInterviewersForDay returns an empty array when the days data is empty", () => {
+  const result = getInterviewersForDay({ days: [] }, "Monday");
+  expect(result.length).toEqual(0);
+});
+
+test("getInterviewersForDay returns an empty array when the day is not found", () => {
+  const result = getInterviewersForDay(state, "Wednesday");
+  expect(result.length).toEqual(0);
+});
+
+
+
+
+
 
 test("getInterview returns an object with the interviewer data", () => {
   const result = getInterview(state, state.appointments["3"].interview);
@@ -95,3 +132,49 @@ test("getInterview returns null if no interview is booked", () => {
   expect(result).toBeNull();
 });
 
+
+
+// back up
+// const state = {
+//   days: [
+//     {
+//       id: 1,
+//       name: "Monday",
+//       appointments: [1, 2, 3]
+//     },
+//     {
+//       id: 2,
+//       name: "Tuesday",
+//       appointments: [4, 5]
+//     }
+//   ],
+//   appointments: {
+//     // Monday
+//     "1": { id: 1, time: "12pm", interview: null },
+//     "2": { id: 2, time: "1pm", interview: null },
+//     "3": {
+//       id: 3,
+//       time: "2pm",
+//       interview: { student: "Archie Cohen", interviewer: 2 }
+//     },
+//     // Tuesday
+//     "4": { id: 4, time: "3pm", interview: null },
+//     "5": {
+//       id: 5,
+//       time: "4pm",
+//       interview: { student: "Chad Takahashi", interviewer: 2 }
+//     }
+//   },
+//   interviewers: {
+//     "1": {  
+//       "id": 1,
+//       "name": "Sylvia Palmer",
+//       "avatar": "https://i.imgur.com/LpaY82x.png"
+//     },
+//     "2": {
+//       id: 2,
+//       name: "Tori Malcolm",
+//       avatar: "https://i.imgur.com/Nmx0Qxo.png"
+//     }
+//   }
+// };
