0afc9d53f9a56e77430b77dd530183f2611287d4
Nick <nsagias@gmail.com>
2021-11-03 00:44:27 -0400

    Add Form component to file, Add CREATE const

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,33 +2,38 @@ import React from 'react';
 import Header from 'components/Appointment/Header';
 import Show from 'components/Appointment/Show';
 import Empty from 'components/Appointment/Empty';
+import Form from 'components/Appointment/Form';
 import useVisualMode from 'hooks/useVisualMode';
 import "components/Appointment/styles.scss"
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
   const {id, item, interview} = props;
   const { mode, transition, back } = useVisualMode(
     interview ? SHOW : EMPTY
   );
+  console.log(mode)
 
   console.log(props)
   return (
     <article className="appointment">
      
       <Header time={props.time} />
-  
-      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      
+      {mode === EMPTY && <Empty onAdd={() => {transition(CREATE)}} />}
+      {mode === CREATE && 
+        <Form onCancel={() => {back()}} />}
       {mode === SHOW && (
         <Show
           student={interview.student}
           interviewer={interview.interviewer}
+          interviewer={interview.interviewer}
         />
       )}
       
-
     </article>
   );
 }
