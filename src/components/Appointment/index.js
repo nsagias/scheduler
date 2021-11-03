1c2ea870c083622baa45c8fa015f21e0d9a05635
Nick <nsagias@gmail.com>
2021-11-02 22:41:58 -0400

    Add useVisualMode and update component render logic

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,23 +5,28 @@ import Empty from 'components/Appointment/Empty';
 import useVisualMode from 'hooks/useVisualMode';
 import "components/Appointment/styles.scss"
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 export default function Appointment(props) {
- const {id, item, interview} = props;
-  // console.log(props.interview.interviewer)
-  // const {interviewer} = interview;
-  // console.log('interviewID:', interviewer.id)
-  // console.log('interviewID:', interviewer.name)
-  // console.log(props)
+  const {id, item, interview} = props;
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  );
+
+  console.log(props)
   return (
     <article className="appointment">
      
       <Header time={props.time} />
   
-        {!interview ? <Empty /> : <Show 
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
+        <Show
           student={interview.student}
           interviewer={interview.interviewer}
-        /> }
+        />
+      )}
       
 
     </article>
