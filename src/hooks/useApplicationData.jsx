5da423cbf1433b544333437cbc98d94f7bc53c5e
Nick <nsagias@gmail.com>
2021-11-05 23:56:12 -0400

    refactor updateSpots to helper folder

--- src/hooks/useApplicationData.jsx
+++ src/hooks/useApplicationData.jsx
@@ -1,35 +1,6 @@
 import React, { useState, useEffect } from "react";
+import updateSpots from "helpers/updateSpots";
 import axios from 'axios';
-
-const updateSpots = function(state, appointments, id) {
-
-  const getNewSpots = (appointments, dayID) => {
-    let count = 0;
-    const foundDay = state.days.find((day) => {
-      return day.id === dayID;
-    });
-
-    foundDay.appointments.forEach(appointment => {
-      const foundAppointment = appointments[appointment];
-      if (foundAppointment.interview === null) {
-        count += 1;
-      } 
-    });
-    return count;
-  }
-
-  const days = state.days.map((day) => {
-    const isCorrectDay = day.appointments.includes(id)
-    if (isCorrectDay) {
-      return { ...day, spots: getNewSpots(appointments, day.id) }
-    } else {
-      return day;
-    }
-  });
-
-  return days;
-};
-
 
 export default function useApplicationData(props) {
   const [state, setState] = useState({
