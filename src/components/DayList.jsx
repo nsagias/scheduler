84a46031b34b3ce1baf5280360c342c3d4710de0
Nick <nsagias@gmail.com>
2021-11-07 02:15:32 -0500

    Remove unused dev code

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import DayListItem from './DayListItem';
-//   Our <DayList> component will take in three props.
-
-// days:Array an array of objects (each object represents a day and includes an id, name, and spots)
-// day:String the currently selected day
-// setDay:Function sets the currently selected day and accepts the name of the day eg. "Monday", "Tuesday
 
 export default function DayList(props) {
   const { days, onChange } = props; 
   const parsedDay = days.map(days => 
-  <DayListItem 
-    key={days.id}
-    name={days.name}
-    spots={days.spots} 
-    selected={days.name === props.value}
-    setDay={onChange}  
-  /> 
+    <DayListItem 
+      key={days.id}
+      name={days.name}
+      spots={days.spots} 
+      selected={days.name === props.value}
+      setDay={onChange}  
+    /> 
   );
+
   return (
     <ul>
       {parsedDay}
