f4fd60689ec1837be2cc167b6eb9135e7f588c94
Nick <nsagias@gmail.com>
2021-11-07 12:41:52 -0500

    Add formating

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -19,15 +19,12 @@ const EDIT = "EDIT";
 const ERROR_SAVE = "ERROR_SAVE";
 const ERROR_DELETE = "ERROR_DELETE";
 
-
-
 export default function Appointment(props) {
   const {id, interview, interviewers, bookInterview, cancelInterview} = props;
   const { mode, transition, back } = useVisualMode(
     interview ? SHOW : EMPTY
   );
   
-
   function save(name, interviewer) {
     const interview = {
       student: name,
