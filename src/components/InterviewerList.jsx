ddfd0acef6a3dd950961f7b94f8d364242c098a0
Nick <nsagias@gmail.com>
2021-11-07 02:22:05 -0500

    Remove dev notes and code, Format code

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -5,10 +5,8 @@ import "components/InterviewerList.scss";
 
 
 export default function InterviewerList(props) {
-  const {interviewers, value, onChange, id} = props;
-  // const [currentInterviewer, setCurrentInterviewer] = useState(0);
-  console.log('InterviewListItem PROPS', props)
-  
+  const {interviewers,onChange} = props;
+
   const parsedInterviewers = interviewers.map(person => 
     <InterviewListItem 
       key={person.id}
@@ -16,32 +14,17 @@ export default function InterviewerList(props) {
       avatar={person.avatar}
       selected={person.id === props.value}
       setInterviewer={() => onChange(person.id)}
-   
     />
-      );
-    // id: 3,
-    //         name: "Sylvia Palmer",
-    //         avatar: "https://i.imgur.com/LpaY82x.png",
-    // const parsedInterviewers = 
-    // <InterviewListItem 
-    //   key={3}
-    //   name={"Sylvia Palmer"}
-    //   avatar={"https://i.imgur.com/LpaY82x.png"}
-    //   selected={3 === value}
-    //   setInterviewer={() => onChange(3)}
-   
-    // />
-
+  );
+ 
   return (
     <section className="interviewers">
-     <h4 className="interviewers__header text--light">Interviewer</h4>
-     <ul className="interviewers__list">{parsedInterviewers}</ul>
-      
-    </section>
-    
-    );
-  }
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{parsedInterviewers}</ul>
+    </section>  
+  );
+}
   
-  InterviewerList.propTypes = {
-    interviewers: PropTypes.array.isRequired
-  };
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
