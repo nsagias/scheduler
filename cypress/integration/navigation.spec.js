c3e74002e6cd14d1e178dc5e6d744a54402d8540
Nick <nsagias@gmail.com>
2021-11-07 00:59:50 -0400

    update test to check for data-testid

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,6 +1,13 @@
 describe("Navigation", () => {
  
-  it("should navigate to Tuesday", () => {
+  it ("should navigate to Tuesday", ()=> {
+    cy.visit("/")
+    cy.get("li").contains("li","Tuesday")
+    .click()
+    .should("have.css", "background-color", "rgb(242, 242, 242)")
+  });
+
+  it("should navigate to Tuesday as selected", () => {
     cy.visit("/");
   
     cy.contains("[data-testid=day]", "Tuesday")
