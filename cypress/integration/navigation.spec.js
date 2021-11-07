4285ed158569f3340e6d1b2fabdc0fe434840b44
Nick <nsagias@gmail.com>
2021-11-07 02:01:18 -0500

    Clean code and lint

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,17 +1,19 @@
 describe("Navigation", () => {
- 
-  it ("should navigate to Tuesday", ()=> {
+
+  it("should navigate to Tuesday", () => {
     cy.visit("/")
-    cy.get("li").contains("li","Tuesday")
-    .click()
-    .should("have.css", "background-color", "rgb(242, 242, 242)")
+
+    cy.get("li").contains("li", "Tuesday")
+      .click()
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
   });
 
+  
   it("should navigate to Tuesday as selected", () => {
     cy.visit("/");
-  
+
     cy.contains("[data-testid=day]", "Tuesday")
       .click()
-      .should("have.class", "day-list__item--selected")
+      .should("have.class", "day-list__item--selected");
   });
 });
