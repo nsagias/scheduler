a3747441721652f717228d745e293b69a4c7b6e3
Nick <nsagias@gmail.com>
2021-11-07 12:45:58 -0500

    Add function level notes

--- src/helpers/updateSpots.js
+++ src/helpers/updateSpots.js
@@ -1,3 +1,12 @@
+/**
+ * Update Spots for current day. (one possible solution)
+ *
+ * @param {Object}   state           State Object.
+ * @param {Object}   appointments    New Appointments array
+ * @param {Object}   id              Optional appointment id (may not need)
+ * @return {Array}   A Days array we can save back into state.
+ */
+
 export default function updateSpots (state, appointments, id) {
 
   const getNewSpots = (appointments, dayID) => {
