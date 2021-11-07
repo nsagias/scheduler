7e4efddbdec1ed91dbb6050dc6482529ab55d5de
Nick <nsagias@gmail.com>
2021-11-07 02:38:58 -0500

    Remove dev notes and repl code

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -4,8 +4,6 @@ export function getAppointmentsForDay(state, day) {
     return appointments;
   }
   
-  // for ( const i of state.days) {  if (i.name === name) { for (const x of i.appointments)  console.log(x) }}
-  // for ( const i of state.days) {  if (i.name === name) { for (const x of i.appointments)   state.appointments[x] }}
   for ( const i of state.days) {  
     if (i.name === day) { 
       for (const x of i.appointments) {   
@@ -19,30 +17,22 @@ export function getAppointmentsForDay(state, day) {
 
 
 export function getInterview(state, interview ){
-  let result = {}
+  let result = {};
   if (interview ===  null) {
     return null;
   }
-// for (const i in state.appointments) { 
-//   result['student'] = state.appointments[3].interview['student']
-//   result['interviewer'] =interviewers[state.appointments[3].interview['interviewer']]
-// }
-// console.log('get interview',interview.interviewer)
-// console.log('student', interview.student)
-// console.log('all the interviews',state.interviewers[interview.interviewer])
 
   result['student'] = interview['student']
   result['interviewer'] = state.interviewers[interview.interviewer];
   
-
-return result;
+  return result;
 };
 
 export function getInterviewersForDay(state, day) {
   let interviewers = [];
   if (state.days.length === 0) {
     return interviewers;
-  }
+  };
  
   for ( const i of state.days) {  
     if (i.name === day) { 
@@ -50,7 +40,7 @@ export function getInterviewersForDay(state, day) {
         interviewers.push(state.interviewers[x]) 
       }
     }
-  }
+  };
 
   return interviewers;
 }
