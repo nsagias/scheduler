a8424c8970853e8fd33fce39cce6908d9af8b9ca
Nick <nsagias@gmail.com>
2021-11-07 01:36:34 -0400

    Add cypress functions and db reset

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,5 +1,14 @@
 describe("Appointments", () => {
-  it ("should nav to /", ()=> {
+  it ("should book an interview", ()=> {
     cy.visit("/")
+    cy.get("li").contains("li","Monday")
+    cy.get("[alt=Add]")
+    .first()
+    .click()
+    .get("[data-testid=student-name-input]").type("Lydia Miller-Jones")
+    cy.get("[alt='Sylvia Palmer']").click()
+    cy.contains("Save").click()
+    cy.request("GET", "localhost:8001/api/debug/reset")
+
   })
 });
