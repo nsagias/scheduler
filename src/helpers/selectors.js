fdc3a46b210b3a583a680434ee6638f6b399bfa3
Nick <nsagias@gmail.com>
2021-11-07 12:40:23 -0500

    Add notes to functions to explain functions

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,9 +1,18 @@
+/**
+ * Function to  get appoment out of state/data
+ * @param {*} state 
+ * @param {*} day 
+ * @returns array of appointments for for day
+ */
 export function getAppointmentsForDay(state, day) {
   let appointments = [];
+  // if appointments are empty return empty array
   if (state.days.length === 0) {
     return appointments;
   }
   
+  // loop over days and if the day matches 
+  // loop over that days appoinment and push onto appointment array
   for ( const i of state.days) {  
     if (i.name === day) { 
       for (const x of i.appointments) {   
@@ -11,29 +20,46 @@ export function getAppointmentsForDay(state, day) {
       }
     }
   }
-
+  // return array of appoints if there are values
   return appointments;
 }
 
+/**
+ * Function to single interviewer
+ * @param {*} state 
+ * @param {*} interview 
+ * @returns  key value pair { student : student, inteviwer: interviwer }
+ */
 
 export function getInterview(state, interview ){
   let result = {};
+  // if interview is null return empty object
   if (interview ===  null) {
     return null;
   }
-
+  // create key/value pair object
   result['student'] = interview['student']
   result['interviewer'] = state.interviewers[interview.interviewer];
   
+  // return key value pair object
   return result;
 };
 
+/**
+ * get interviewers for day
+ * @param {*} state 
+ * @param {*} day 
+ * @returns array of inteviwers for day
+ */
 export function getInterviewersForDay(state, day) {
   let interviewers = [];
+  // check to see if interviwers on day, return empty array if none
   if (state.days.length === 0) {
     return interviewers;
   };
  
+  // loop through days and check for specific day
+  // they loop thorugh each interview and append to array
   for ( const i of state.days) {  
     if (i.name === day) { 
       for (const x of i.interviewers) {   
@@ -42,5 +68,6 @@ export function getInterviewersForDay(state, day) {
     }
   };
 
+  // return array of interviewers
   return interviewers;
 }
