4bc0c6f59e5785c03a8d8da22cb7dfd083f8b5f6
Nick <nsagias@gmail.com>
2021-11-07 02:39:58 -0500

    Remove dev notes

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -5,8 +5,6 @@ import {
 } from "helpers/selectors";
 
 
-
-
 const state = {
   days: [
     {
@@ -110,9 +108,6 @@ test("getInterviewersForDay returns an empty array when the day is not found", (
 
 
 
-
-
-
 test("getInterview returns an object with the interviewer data", () => {
   const result = getInterview(state, state.appointments["3"].interview);
   expect(result).toEqual(
@@ -132,49 +127,3 @@ test("getInterview returns null if no interview is booked", () => {
   expect(result).toBeNull();
 });
 
-
-
-// back up
-// const state = {
-//   days: [
-//     {
-//       id: 1,
-//       name: "Monday",
-//       appointments: [1, 2, 3]
-//     },
-//     {
-//       id: 2,
-//       name: "Tuesday",
-//       appointments: [4, 5]
-//     }
-//   ],
-//   appointments: {
-//     // Monday
-//     "1": { id: 1, time: "12pm", interview: null },
-//     "2": { id: 2, time: "1pm", interview: null },
-//     "3": {
-//       id: 3,
-//       time: "2pm",
-//       interview: { student: "Archie Cohen", interviewer: 2 }
-//     },
-//     // Tuesday
-//     "4": { id: 4, time: "3pm", interview: null },
-//     "5": {
-//       id: 5,
-//       time: "4pm",
-//       interview: { student: "Chad Takahashi", interviewer: 2 }
-//     }
-//   },
-//   interviewers: {
-//     "1": {  
-//       "id": 1,
-//       "name": "Sylvia Palmer",
-//       "avatar": "https://i.imgur.com/LpaY82x.png"
-//     },
-//     "2": {
-//       id: 2,
-//       name: "Tori Malcolm",
-//       avatar: "https://i.imgur.com/Nmx0Qxo.png"
-//     }
-//   }
-// };
